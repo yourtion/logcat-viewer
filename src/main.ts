317b001020378742cbbe6cf34dccd856a137a13b
YourtionGuo <yourtion@gmail.com>
2022-08-09 22:09:25 +0800

    feat: add @tauri-apps/api and call from js

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import { invoke } from '@tauri-apps/api'
+
+invoke('greet', { name: 'Tauri' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
